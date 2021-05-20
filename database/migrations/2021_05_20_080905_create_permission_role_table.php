e54861cdd82029e75acd7d67fcf931d1db71b0da
sangart <sang.do.van1001@gmail.com>
2021-05-20 15:30:37 +0700

    edit databasse

--- database/migrations/2021_05_20_080905_create_permission_role_table.php
+++ database/migrations/2021_05_20_080905_create_permission_role_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePermissionRolesTable extends Migration
+class CreatePermissionRoleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreatePermissionRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('permission_roles', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('role_id');
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->id();   
             $table->unsignedBigInteger('permission_id');
+            $table->unsignedBigInteger('role_id');
             $table->timestamps();
 
-            $table->foreign('role_id')->references('id')->on('roles');
             $table->foreign('permission_id')->references('id')->on('permissions');
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
@@ -31,6 +31,6 @@ class CreatePermissionRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('permission_roles');
+        Schema::dropIfExists('permission_role');
     }
 }
